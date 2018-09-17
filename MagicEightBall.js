e3c09204ed69e7e88eebb68f85d84600beb7392c
Kendall Berner <KB057215@cerner.net>
2018-09-17 13:52:48 -0500

    Magic Eight Ball

---
+++ MagicEightBall.js
@@ -0,0 +1,5 @@
+//Giving advice for cerner_2^5_2018
+
+var messages = ["Yes", "No", "Maybe", "Ask again later", "Better not tell you now", "Don't count on it", "Without a doubt", "It is certain"];
+var i = Math.floor(Math.random() * 8);
+console.log(messages[i]);
